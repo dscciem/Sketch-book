c538c6be8289be5debb08a108d4562a4a5673a10
Sagnik Chattopadhyaya <meshagy18@gmail.com>
2020-09-21 02:17:22 +0530

    Pen features addition

--- drawing_book.py
+++ drawing_book.py
@@ -12,8 +12,15 @@ class Paint(object):
         self.root.maxsize(1600,900)
         self.root.minsize(500,300)
 
-        self.paint_tools = Frame(self.root,width = 100,height = 100,relief = RIDGE,borderwidth = 3)
+        self.paint_tools = Frame(self.root,width = 100,height = 300,relief = RIDGE,borderwidth = 3)
         self.paint_tools.place(x=0,y=0)
+
+        self.pen_logo = ImageTk.PhotoImage(Image.open('logos/pen.png'))
+        self.p = Label(self.paint_tools , text = 'Pen' , borderwidth = 0, font = ('verdana',10,'bold'))
+        self.p.place(x=5,y=10)
+        self.pen_button = Button(self.paint_tools,padx=6,image = self.pen_logo,borderwidth=2,command = self.use_pen)
+        self.pen_button.place(x=60,y=10)
+
 
 
 if __name__ == '__main__':
