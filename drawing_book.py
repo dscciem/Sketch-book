5e67ce9490fc5ef5dbc3a36351bd5d64184ec6b2
Sagnik Chattopadhyaya <meshagy18@gmail.com>
2020-09-23 20:49:46 +0530

    choose color

--- drawing_book.py
+++ drawing_book.py
@@ -66,9 +66,12 @@ class Paint(object):
         self.activate_button(self.brush_button)
 
     def use_eraser(self):
-        self.activate_button(self.eraser_button,eraser_on=True)
+        self.activate_button(self.eraser_button,eraser_mode=True)
 
-    
+    def choose_color(self):
+        self.eraser_on = False
+        self.color = askcolor(color=self.color)[1]
+
 
 
 if __name__ == '__main__':
